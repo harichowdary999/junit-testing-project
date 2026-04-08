29c86e417d9855281f751f519a6c2528d019b2ea
D. Hari charan <darapanenihari29@gmail.com>
2026-04-08 06:14:57 +0000

    JUnit project added

---
+++ junit-project/src/test/java/CalculatorTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    void testAddition() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    void testSubtraction() {
+        Calculator calc = new Calculator();
+        assertEquals(1, calc.subtract(3, 2));
+    }
+}
